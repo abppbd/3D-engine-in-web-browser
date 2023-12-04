d7e44889f57b73d9d5825c1803589603d89f977b
abppbd <148499157+abppbd@users.noreply.github.com>
2023-12-04 12:06:38 +0100

    Update generate json.py - completed points, edges & faces - better structure

--- js/generate json.py
+++ js/generate json.py
@@ -3,42 +3,40 @@
 
 """
 JSON Structure:
+-------------------------------------------------------------------------------
 For rendering points:
 
 [shape1(a), shape2(a)...]
-+-shape: [Id(n), name(s), *color(h)*, type(s), points(a)]
-| +-points: [p1(a), p2(a)...]
-| | +-p: [*color(h)*, coords(a)]
-| | | +-coords: [x(n), y(n), z(n)]
-
-
-
-[shape1(a), shape2(a)...]
- V
- [Id(n), name(s), *color(h)*, type(s), points(a)]
-                                       v--------
-                               [p1(a), p2(a)...]
-                                v----
-                     [*color(h)*, coords(a)]
-                                  v--------
-                          [x(n), y(n), z(n)]
+ +--shape: [Id(n), name(s), type(s), render(b), load(b), *color(h)*, points(a)]
+ |  +--points: [p1(a), p2(a)...]
+ |  |  +--p: [*color(h)*, coords(a)]
+ |  |  |  +--coords: [x(n), y(n), z(n)]
 
 -------------------------------------------------------------------------------
 For rendering edges:
 
 [shape1(a), shape2(a)...]
- V
- [Id(n), name(s), *color(h)*, type(s), points(a), edges]
-                                       v--------  v----
-                       [p1(a), p2(a)...]          [e1(a), e2(a), e3(a)...]
-                        v----                      v----
-  [*color(h)*, coords(a)]                   [*color(h)*, points(s)]
-               v--------                                 v-----
-       [x(n), y(n), z(n)]                               "p1-p2"
-                                                        (points index)
+ +--shape: [Id(n), name(s), type(s), render(b), load(b), *color(h)*, points(a), edges(a)]
+ |  +--points: [p1(a), p2(a)...]
+ |  |  +--p: [*color(h)*, coords(a)]
+ |  |  |  +--coords: [x(n), y(n), z(n)]
+ |  |
+ |  +--edges: [e1, e2...]
+ |  |  +--e: [*color(h)*, points(a)]
+ |  |  |  +--points: [p1Index(n), p2Index(n)]
 
+-------------------------------------------------------------------------------
+For rendering faces:
 
-
+[shape1(a), shape2(a)...]
+ +--shape: [Id(n), name(s), type(s), *render(b)*, *load(b)*, *color(h)*, points(a), faces(a)]
+ |  +--points: [p1(a), p2(a)...]
+ |  |  +--p: [*color(h)*, coords(a)]
+ |  |  |  +--coords: [x(n), y(n), z(n)]
+ |  |
+ |  +--faces: [f1, f2...]
+ |  |  +--f: [*color(h)*, points(a)]
+ |  |  |  +--points: [p1Index(n), p2Index(n), p3Index(n)]
 
 
 legend:
